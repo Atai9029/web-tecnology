64343465b9c810c7098199928503e3d487c8ebad
Smoky Nagato <makhmutov_a@iuca.kg>
2026-04-10 09:34:55 +0600

    Delete www directory

--- www/JS/transports.js
+++
@@ -1,9 +0,0 @@
-function changeImage(img) {
-    const mainImage = document.getElementById("mainImage");
-    mainImage.src = img.src;
-
-    const thumbs = document.querySelectorAll(".thumbs img");
-    thumbs.forEach(item => item.classList.remove("active"));
-
-    img.classList.add("active");
-}
